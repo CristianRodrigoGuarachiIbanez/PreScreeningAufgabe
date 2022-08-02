8439c47605ca3b1e7f0f82bb4c63020d221fd078
CristianRodrigoGuarachiIbanez <cristguarachi@gmail.com>
2022-08-02 15:53:58 +0200

    src

--- test/test_screening_aufgabe.py
+++ test/test_screening_aufgabe.py
@@ -3,25 +3,26 @@ import argparse
 import pytest
 import os
 
+
 def current_time(heute, index):
     heute = heute.split(" ")
     today = heute[0].split(".")
-    today = today[2] +"."+ today[1]+"."+today[0]
+    today = today[2] + "." + today[1] + "." + today[0]
     zeit = heute[1].split(":")
-    hh,mm,ss = int(zeit[0]),int(zeit[1]),0
+    hh, mm = int(zeit[0]), int(zeit[1])
     output = []
     for i in range(index):
-        if(mm>=59):
-            mm=00
-            if(hh==24 or hh==00):
-                hh=1
+        if (mm >= 59):
+            mm = 00
+            if (hh == 24 or hh == 00):
+                hh = 1
             else:
                 hh += 1
         else:
-            mm +=2
-            if(mm>=59):
+            mm += 2
+            if (mm >= 59):
                 mm = 00
-        hour = today + " " + str(hh)+":"+str(mm)+":"+zeit[2] +"\n"
+        hour = today + " " + str(hh) + ":" + str(mm)+":" + zeit[2] + "\n"
         output.append(hour)
     return output
 
@@ -29,7 +30,7 @@ def current_time(heute, index):
 def create_examples(index=1):
     path = os.path.dirname(os.path.realpath(__file__))
     print(path)
-    filename=path + "/example"
+    filename = path + "/example"
     name = filename + str(index) + ".txt"
     today = datetime.today().strftime('%Y.%m.%d %H:%M:%S')
     examples = current_time(today, index)
@@ -38,9 +39,10 @@ def create_examples(index=1):
             file.write(line)
         file.close()
 
+
 def _make_sleep(num=None, filename=None):
 
-    if(num is None):
+    if (num is None):
         num = input("Wie viele Daten möchten Sie eingeben? ")
         num = get_input_number(num)
 
@@ -48,15 +50,15 @@ def _make_sleep(num=None, filename=None):
 
     if (filename is None):
         path = os.path.dirname(os.path.realpath(__file__))
-        if(num==1):
+        if (num == 1):
             filename = path + "/example1.txt"
-        elif(num==5):
+        elif (num == 5):
             filename = path + "/example5.txt"
-        elif(num==10):
+        elif (num == 10):
             filename = path + "/example10.txt"
         elif (num == 15):
             filename = path + "/example15.txt"
-        elif(num==20):
+        elif (num == 20):
             filename = path + "/example20.txt"
         else:
             print("Bitte geben Sie eine der folgengen Zahlen 1, 5, 10, 15, 20 ein")
@@ -67,12 +69,13 @@ def _make_sleep(num=None, filename=None):
         seks, dates = get_days(num, line)
         make_sleep(seks, dates)
 
-@pytest.mark.parametrize("num", [1,5])
+
+@pytest.mark.parametrize("num", [1, 5])
 def test_make_sleep(num):
     _make_sleep(num)
 
 
-if(__name__ == "__main__"):
+if (__name__ == "__main__"):
     parser = argparse.ArgumentParser()
     parser.add_argument("-dates", "--num_dates", type=int)
     args = parser.parse_args()
